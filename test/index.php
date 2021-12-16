61fd214f183f3c9b94e347d40ece1a573483add3
CesarCalad833 <cesar.augusto9782@gmail.com>
2021-12-16 17:02:45 -0500

    Test confirmados y en funcionamiento

--- test/index.php
+++ test/index.php
@@ -1,8 +1,9 @@
 <?php
 require "../GenerateSiteMap1e.php";
 $GSM1e = new GenerateSiteMap1e();
-$baseRoute = "localhost/GenerateSiteMap1e/test/";
-$GSM1e->addUrl($baseRoute . 'noExisto.php');
+$baseRoute = "http://localhost/GenerateSiteMap1e/test/";
+$GSM1e->addUrl($baseRoute . 'index.php');
+$GSM1e->addUrl($baseRoute . 'example.html');
 $GSM1e->save();
 echo "File create!";
 ?>
