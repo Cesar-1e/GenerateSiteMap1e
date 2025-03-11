bd8129e87f99b688351170f531159a68a95bc6e6
Cesar 1e <cesar.augusto9782@gmail.com>
2025-03-11 04:52:36 -0500

    Se implementa validaciones y mejora de escritura en el archivo sitemap.xml

--- GenerateSiteMap1e.php
+++ GenerateSiteMap1e.php
@@ -1,53 +1,78 @@
 <?php
-class GenerateSiteMap1e{
+class GenerateSiteMap1e
+{
     private $file = null;
     private $urls = array();
     private $xmlns = "http://www.sitemaps.org/schemas/sitemap/0.9";
 
     public function __construct($route = null)
     {
-        if(is_null($route)){
+        if (is_null($route)) {
             $route = dirname(dirname(__FILE__)) . "/sitemap.xml";
         }
         $this->open($route);
     }
 
-    public function getXmlns(){
+    public function getXmlns()
+    {
         return $this->xmlns;
     }
 
-    public function setXmlns($xmlns){
+    public function setXmlns($xmlns)
+    {
         $this->xmlns = $xmlns;
     }
-    
-    private function open($route){
-        //Obtenemos el contenido
+
+    private function open($route)
+    {
+        if (!file_exists($route)) {
+            $this->file = fopen($route, "w+");
+            if ($this->file === false) {
+                throw new Exception('No pudimos abrir el archivo: ' . $route);
+            }
+            return;
+        }
+
         $xml = file_get_contents($route);
-        if($xml != "" && $xml !== false){
+        if ($xml !== false && !empty($xml)) {
             $xml = simplexml_load_string($xml, "SimpleXMLElement", LIBXML_NOCDATA);
+            if ($xml === false) {
+                throw new Exception('Error al cargar el XML: ' . $route);
+            }
             $json = json_encode($xml);
-            $this->urls = json_decode($json,TRUE)["url"];
+            $this->urls = json_decode($json, TRUE)["url"];
         }
 
         $this->file = fopen($route, "w+");
-        if($this->file === false){
+        if ($this->file === false) {
             throw new Exception('No pudimos abrir el archivo: ' . $route);
         }
     }
 
-    public function addUrl($loc, $lastmod = null){
-        if($lastmod == null){
+    public function addUrl($loc, $lastmod = null)
+    {
+        if (!filter_var($loc, FILTER_VALIDATE_URL)) {
+            throw new Exception('URL no válida: ' . $loc);
+        }
+
+        if ($lastmod == null) {
             $lastmod = date('Y-m-d');
         }
+
         $this->urls[] = array(
             'loc' => $loc,
             'lastmod' => $lastmod
         );
     }
 
-    public function updateUrl($previousLoc, $nextLoc){
+    public function updateUrl($previousLoc, $nextLoc)
+    {
+        if (!filter_var($nextLoc, FILTER_VALIDATE_URL)) {
+            throw new Exception('URL no válida: ' . $nextLoc);
+        }
+
         $key = array_search($previousLoc, array_column($this->urls, 'loc'));
-        if($key !== false){
+        if ($key !== false) {
             $this->urls[$key] = array(
                 'loc' => $nextLoc,
                 'lastmod' => date('Y-m-d')
@@ -55,19 +80,24 @@ class GenerateSiteMap1e{
         }
     }
 
-    public function deleteUrl($loc){
+    public function deleteUrl($loc)
+    {
         $key = array_search($loc, array_column($this->urls, 'loc'));
-        unset($this->urls[$key]);
+        if ($key !== false) {
+            unset($this->urls[$key]);
+            $this->urls = array_values($this->urls); // Reindex array
+        }
     }
 
-    public function save(){
+    public function save()
+    {
         $header = '<?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="' . $this->xmlns . '">';
         $body = '';
         foreach ($this->urls as $url) {
             $body .= '<url>';
             foreach ($url as $key => $value) {
-                $body .= '<' . $key . '>' . $value . '</' . $key . '>';
+                $body .= '<' . $key . '>' . htmlspecialchars($value, ENT_XML1, 'UTF-8') . '</' . $key . '>';
             }
             $body .= '</url>';
         }
@@ -76,7 +106,10 @@ class GenerateSiteMap1e{
         $this->close();
     }
 
-    private function close(){
-        fclose($this->file);
+    private function close()
+    {
+        if ($this->file !== null) {
+            fclose($this->file);
+        }
     }
 }
--- test/index.php
+++ test/index.php
@@ -1,9 +1,11 @@
 <?php
 require "../GenerateSiteMap1e.php";
-$GSM1e = new GenerateSiteMap1e();
+$GSM1e = new GenerateSiteMap1e("/var/www/html/GenerateSiteMap1e/test/sitemap.xml");
 $baseRoute = "http://localhost/GenerateSiteMap1e/test/";
 $GSM1e->addUrl($baseRoute . 'index.php');
 $GSM1e->addUrl($baseRoute . 'example.html');
+/*$GSM1e->updateUrl($baseRoute . 'index.php', $baseRoute . 'index2.php');
+$GSM1e->deleteUrl($baseRoute . 'example.html');*/
 $GSM1e->save();
 echo "File create!";
 ?>
